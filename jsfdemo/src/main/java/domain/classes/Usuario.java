5ec769a3572d4b68a22b86ce55b12cc300c0f0ab
rvazquez <rvazquez@siga.com.mx>
2012-01-05 17:23:59 -0600

    Primer commit.

---
+++ jsfdemo/src/main/java/domain/classes/Usuario.java
@@ -0,0 +1,20 @@
+package domain.classes;
+
+public class Usuario {
+		private String nombre;
+		private String contrasenia;
+		
+		public String getNombre() {
+			return nombre;
+		}
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+		public String getContrasenia() {
+			return contrasenia;
+		}
+		public void setContrasenia(String contrasenia) {
+			this.contrasenia = contrasenia;
+		}
+		
+}
