d0a3c66bf625a9d47d2b2d4ed5143b85f2a19122
rvazquez <rvazquez@siga.com.mx>
2012-01-06 13:27:06 -0600

    primer prototipo

---
+++ jsfdemo/src/main/java/domain/classes/Libro.java
@@ -0,0 +1,25 @@
+package domain.classes;
+
+public class Libro {
+	private Integer id;
+	private String titulo;
+	private String autor;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+}
